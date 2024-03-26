b72dd72cc7319bb86627d9947f5a1a53acfab35f
Nilam3993 <nilamballal007@gmail.com>
2024-03-26 12:41:59 +0530

    add this all file

---
+++ Ques02/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.42.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+}
+
+
+resource "aws_instance" "this_ec2" {
+    ami = "ami-05295b6e6c790593e"
+    instance_type = "t2.micro"
+    key_name = "aws_web_key"
+  
+}
